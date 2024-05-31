e0d3e2fa6de2d418313558827100a6d08fefd649
thomasstanly <thomasstanly12@gmail.com>
2024-05-31 16:20:35 +0530

    removed django steup

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -15,8 +15,6 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from celery.schedules import crontab
 load_dotenv()
-import django
-django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
