00e85c24881f66ce291c9547fbb4f09de37ae8b5
thomasstanly <thomasstanly12@gmail.com>
2024-05-31 15:56:43 +0530

    WASGI command

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -8,15 +8,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
+import django
+django.setup()
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 from chat import routing
-import django
-django.setup()
-git
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 django_application_asgi = get_asgi_application()
