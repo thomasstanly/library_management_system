a6857f8e473e6356016f98ad420bebc1fef3f120
thomasstanly <thomasstanly12@gmail.com>
2024-05-31 16:04:40 +0530

    collectstatic added

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -179,6 +179,7 @@ CELERY_BEAT_SCHEDULE = {
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
